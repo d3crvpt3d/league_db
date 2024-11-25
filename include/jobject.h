7c5987179177a41aae401593256b6ed475386672
d3crvpt3d <d3crvpt3d@gmail.com>
2024-11-25 21:19:39 +0100

    +main branch

--- include/jobject.h
+++ include/jobject.h
@@ -5,18 +5,18 @@ union Value {
 	int 	as_integer;
 	float 	as_decimal;
 	char 	*as_string;
-}
+};
 
-struct JStack {
+struct JObject {
 	char *key;
 	union Value value;
 
 	//dyn alloc
-	struct JStack *childs;
-}
+	struct JObject *childs;
+};
 
 //malloc's itself with key
-struct JStack *createJStack(char *key);
+struct JObject *createJObject(char *key);
 
 //free's recusively childs and itself
-deleteJStack(struct JStack *jstack);
+int deleteJObject(struct JObject *jobject);
---
+++ src/jobject.c
@@ -0,0 +1,9 @@
+#include "../include/jobject.h"
+
+struct JObject *createJObject(char *key){
+	return 0;
+}
+
+int deleteJObject(struct JObject *jobject){
+	return 0;
+}
