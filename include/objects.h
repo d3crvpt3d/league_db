e5019c414cc140f531262e672c1694965c47f8ff
d3crypt3d <st4r.sprayer@gmail.com>
2024-11-25 12:12:49 +0100

    smth

---
+++ include/objects.h
@@ -0,0 +1,5 @@
+#pragma once
+
+typedef struct JsonObj {
+	data
+} JsonObj;
--- update_items.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-VERSION=$(curl -s https://ddragon.leagueoflegends.com/api/versions.json | jq .[0] -r)
-
-curl -s https://ddragon.leagueoflegends.com/cdn/${VERSION}/data/en_US/item.json > src/items.json
