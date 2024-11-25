adac1774cc1554c9b40366a9163987f7dbc68909
d3crypt3d <st4r.sprayer@gmail.com>
2024-11-25 21:30:50 +0100

    save

--- src/jobject.c
+++ src/jobject.c
@@ -1,9 +1,22 @@
 #include "../include/jobject.h"
+#include <stdlib.h>
 
 struct JObject *createJObject(char *key){
+	
+	struct JObject *this = malloc(sizeof(struct JObject));
+
+	//TODO
+
 	return 0;
 }
 
+//free childs, then yourself (recursive)
 int deleteJObject(struct JObject *jobject){
+	
+	while(1){
+		//TODO
+	}
+
+	free(jobject);
 	return 0;
 }
